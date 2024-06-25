eeb68000946f56a98b0f094db1f88e4142cee289
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-06-24 17:59:08 -0700

    New Font + Positioning adjustments

--- fetchit_kids/app/page.tsx
+++ fetchit_kids/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
-import { Nunito } from "next/font/google";
+import { Nunito, Press_Start_2P } from "next/font/google";
 import avatar1 from "../public/av1.png";
 import avatar2 from "../public/av2.png";
 import avatar3 from "../public/av3.png";
 import star from "../public/star.png";
-import fetchit_logo from  "../public/fetchit.png"
+import fetchit_logo from "../public/fetchit.png";
+
 const nunito = Nunito({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const p2p = Press_Start_2P({
   weight: "400",
   subsets: ["latin"],
 });
@@ -13,73 +19,103 @@ const nunito = Nunito({
 export default function Home() {
   return (
     <main
-      className={`${nunito.className} relative bg-[#fefcf7] min-h-screen flex flex-col items-center `}
+      className={`${nunito.className} bg-[#fefcf7] flex flex-col items-center justify-between`}
     >
-      <div className="flex flex-row items-center w-[100%] justify-between px-[5%]">
-        <div className="tilted-image-container-1">
-          <Image
-            src={star}
-            alt="Tilted Star"
-            layout="fill"
-            objectFit="contain"
-            className="tilted-image"
-          />
+      {/* body */}
+      <div className="flex flex-col w-[100%] items-center justify-center h-[100vh] pb-[15%]">
+        <div className="flex flex-row items-center w-[100%] justify-between px-[5%]">
+          <div className="tilted-image-container-1">
+            <Image
+              src={star}
+              alt="Tilted Star"
+              layout="fill"
+              objectFit="contain"
+              className="tilted-image"
+            />
+          </div>
+          <div className="inline-block rounded-full text-white bg-[#818cf8] p-5">
+            <p className="font-extrabold text-3xl">
+              Learn Something New Everyday
+            </p>
+          </div>
+          <div className="tilted-image-container-2">
+            <Image
+              src={star}
+              alt="Tilted Star"
+              layout="fill"
+              objectFit="contain"
+              className="tilted-image"
+            />
+          </div>
         </div>
-        <div className="inline-block rounded-full text-white bg-[#818cf8] p-5">
-          <p className="font-extrabold text-3xl">
-            Learn Something New Everyday
-          </p>
-        </div>
-        <div className="tilted-image-container-2">
-          <Image
-            src={star}
-            alt="Tilted Star"
-            layout="fill"
-            objectFit="contain"
-            className="tilted-image"
-          />
-        </div>
-      </div>
 
-      <div className="inline-block rounded-full text-white bg-[#4ade80] p-5 mb-4">
-        <p className="font-extrabold text-3xl">Meet your guides</p>
-      </div>
+        <div className="inline-block rounded-full text-white bg-[#4ade80] p-5 mb-4">
+          <p className="font-extrabold text-3xl">Meet your guides</p>
+        </div>
 
-      <div className="flex justify-center space-x-40 w-[70%] ">
-        <div className="">
-          <div className="bg-[#b45309] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
-            <Image src={avatar1} alt="Description of the image"></Image>
+        <div className="flex justify-center space-x-40 w-[70%]">
+          <div className="">
+            <div
+              className={`Press_Start_2P bubble shadow mini bottom ${p2p.className}`}
+            >
+              Hello! I'm Ross
+            </div>
+            <div className="bg-[#b45309] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
+              <Image src={avatar1} alt="Description of the image"></Image>
+            </div>
+            <div className="flex items-center justify-center text-2xl font-extrabold mt-2">
+              <button className="flex items-center justify-center text-2xl font-extrabold bg-[#b45309] rounded-full mt-2 p-2 text-white">
+                Click To chat
+              </button>
+            </div>
           </div>
-          <div className="flex items-center justify-center text-4xl font-extrabold bg-[#b45309] rounded-full mt-2">
-            Ross
+          <div className="">
+            <div
+              className={`Press_Start_2P bubble shadow mini bottom ${p2p.className}`}
+            >
+              Hello! I'm Joey
+            </div>
+            <div className="flex bg-[#4ade80] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
+              <Image src={avatar2} alt="Description of the image"></Image>
+            </div>
+            <div className="flex items-center justify-center text-2xl font-extrabold mt-2">
+              <button className="flex items-center justify-center text-2xl font-extrabold bg-[#4ade80] rounded-full mt-2 p-2 text-white">
+                Click To chat
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="">
-          <div className="bg-[#4ade80] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
-            <Image src={avatar2} alt="Description of the image"></Image>
-          </div>
-          <div className="flex items-center justify-center text-4xl font-extrabold bg-[#4ade80] rounded-full mt-2">
-            Joey
-          </div>
-        </div>
-        <div className="">
-          <div className="bg-[#818cf8] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
-            <Image src={avatar3} alt="Description of the image"></Image>
-          </div>
-          <div className="flex items-center justify-center text-4xl font-extrabold bg-[#818cf8] rounded-full mt-2">
-            Chandler
+          <div className="">
+            <div
+              className={`Press_Start_2P bubble shadow mini bottom ${p2p.className}`}
+            >
+              Hello! I'm Chandler
+            </div>
+            <div className="bg-[#818cf8] w-[300px] h-[300px] rounded-full transition-transform transform hover:scale-110">
+              <Image src={avatar3} alt="Description of the image"></Image>
+            </div>
+            <div className="flex items-center justify-center text-2xl font-extrabold mt-2">
+              <button className="flex items-center justify-center text-2xl font-extrabold bg-[#818cf8] rounded-full mt-2 p-2 text-white">
+                Click To chat
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      <footer className="bg-[#fefcf7] absolute bottom-0 shadow w-[100%] border-2 border-t-[#3E4EA1] ">
+      {/* footer element */}
+      <footer className="bg-red-100 w-[100%]">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="flex items-center">
-          <Image src={fetchit_logo} alt="Fetchit logo" width={50} height={50} />
-          <span className="mt-[15px] font-inter font-bold text-[#3E4EA1] ml-1 text-[37px] leading-[50px] tracking-wider">
-            Fetchit
-          </span>
-        </div>
+            <div className="flex items-center">
+              <Image
+                src={fetchit_logo}
+                alt="Fetchit logo"
+                width={50}
+                height={50}
+              />
+              <span className="mt-[15px] font-inter font-bold text-[#3E4EA1] ml-1 text-[37px] leading-[50px] tracking-wider">
+                Fetchit
+              </span>
+            </div>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 ">
               <li>
                 <a href="#" className="hover:underline me-4 md:me-6">
